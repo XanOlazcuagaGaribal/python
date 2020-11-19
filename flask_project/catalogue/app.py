c79eae63f65e85f2ba881048e689541cc101af08
XanOlazcuagaGaribal <72494567+XanOlazcuagaGaribal@users.noreply.github.com>
2020-11-19 11:40:59 +0100

    Recherche par id et titre ok

--- flask_project/catalogue/app.py
+++ flask_project/catalogue/app.py
@@ -4,48 +4,44 @@ import json
 
 app = Flask(__name__)
 
-books=[
-  {
-      'id':1,
-      'titre' : 'un titre',
-  },
-  {
-      'id':2,
-      'titre': 'un autre titre random',
-  }
-]
+def library():
+    books=[
+        {
+            'id':1,
+            'titre' : 'un titre',
+        },
+        {
+            'id':2,
+            'titre': 'un autre titre random',
+        }
+    ]
+    return books
 
 @app.route("/")
 def index():
  return "Hello DC!"
 
 @app.route("/api/books", methods = ['GET'])
-def books():
-    return jsonify(book)
+def books_liste():
+    books = library()
+    return jsonify(books)
 
-@app.route("/api/book/<int:id>",methods = ['GET'])
-def return_id(id):
-    results=[]
+@app.route("/api/book/<int:book_id>",methods = ['GET'])
+def return_id(book_id=None):
+    books = library()
     for book in books:
-        if book['id'] == id:
-            results.append(book)
-            return jsonify(results)
-        else:
-            return "No books found"
-    
+        if book['id'] == int(book_id):
+            selected = book
+    return selected    
 
 #Ne marche pas
 @app.route("/api/book/<path:titre>")
 def return_titre(titre):
-    results=[]
+    books = library()
     for book in books:
-        if book['id'] == id:
-            results.append(book)
-            return jsonify(results)
-        else:
-            return "No books found"
-            
-    #return jsonify(book['title'])
+        if book['titre'] == titre:
+            selected = book
+    return selected    
 
 @app.route("/api/books/titre")
 def hello(name):
