2b2533b678c0c56f926e64ec3269e3d12ba8d1ae
XanOlazcuagaGaribal <72494567+XanOlazcuagaGaribal@users.noreply.github.com>
2020-12-02 12:32:31 +0100

    Ajout d'un todo ok, début affichage des todos

--- Django/TP_Django/todo/views.py
+++ Django/TP_Django/todo/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.template import loader
 
 from .models import Task
+from datetime import date
 # Create your views here.
 
 def index(request):
-    return render(request, 'todo/index.html')
+    todos = Task.objects.all() #On récupère tous les todos
+
+    if request.method == "POST":
+        if "addTask" in request.POST:
+            content = request.POST["inputTaskContent"]
+            is_done = False
+            created_date = date.today()
+            Todo = Task(content=content, is_done=is_done, created_date=created_date)
+            Todo.save()
+            return redirect("/todo")
+
+    return render(request, 'todo/index.html',{"todos":todos})
+
